391d1e2aab0422fe7aa73d2f1b65023dd0448044
Luke Morgan <morgan.luke94@gmail.com>
2020-08-31 17:02:22 +0100

    adds ip address logging, and db connection error handling

--- app/api/middleware/LoggingMiddleWare.php
+++ app/api/middleware/LoggingMiddleWare.php
@@ -1,22 +1,23 @@
-<?php 
+<?php
 
 namespace CovidDashboard\App\Api\Middleware;
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-class AppLoggingMiddleware {
+class AppLoggingMiddleware
+{
 
   public function __construct($app_container)
   {
     $this->slim_container = $app_container;
   }
 
-  public function __invoke(Request $request, Response $response, $next) {
-    $uri = $request->getUri();    
-    $this->slim_container->logger->addInfo('request-method: '.$request->getMethod().', full path requested: '.$request->getUri().', host: '.$uri->getHost().', port: '.$uri->getPort());
+  public function __invoke(Request $request, Response $response, $next)
+  {
+    $uri = $request->getUri();
+    $this->slim_container->logger->addInfo('request-method: ' . $request->getMethod() . ', full path requested: ' . $request->getUri() . ', host: ' . $uri->getHost() . ', port: ' . $uri->getPort() . ', user: ip: ' . $request->getAttribute('ip_address'));
     $response = $next($request, $response);
     return $response;
   }
-  
 }
