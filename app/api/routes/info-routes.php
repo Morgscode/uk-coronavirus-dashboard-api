8a90b0bac009d629e5f2925258892e066edfbc82
Luke Morgan <morgan.luke94@gmail.com>
2020-08-31 17:52:47 +0100

    remove unused dev routes

--- app/api/routes/info-routes.php
+++ app/api/routes/info-routes.php
@@ -1,11 +1,3 @@
 <?php
 
 $api->slim_app->get('/info/nhs-covid-19', 'DashboardWidgetController:getNHSCovidInfo');
-
-if (ENV === 'dev') :
-
-  $api->slim_app->get('/info/nhs-covid-api-test', 'DashboardWidgetController:testApiResponse');
-
-  $api->slim_app->get('/info/update-covid-api-info', 'DashboardWidgetController:updateNHSCovidInfo');
-
-endif;
